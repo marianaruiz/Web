a2bac3baaec10694ca02a598f5da3ea212b3d8e2
Mariana <Mariana@Mariana>
2015-03-06 19:28:49 -0600

    pojos

---
+++ src/java/model/Alumno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Mariana
+ */
+
+public class Alumno{
+    private String Boleta;
+    private String nombre;
+    private String paterno;
+    private String materno;
+    
+    private String email;
+    
+}
